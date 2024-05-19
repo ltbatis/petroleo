8a263f72a96c99888c13e7f028970f37796ad996
lucas <ltbatis@proton.me>
2024-05-19 15:30:28 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
+import pandas as pd
 import streamlit as st
 
+url = 'https://drive.google.com/uc?id=1-KpcABxWug0gfsnk_h3DgtWjN-3tOv8H'
+
 st.write("Carregando dados...")
+dados = pd.read_parquet(url)
+
+st.write("Amostra dos dados:")
+st.dataframe(dados.sample(10))
